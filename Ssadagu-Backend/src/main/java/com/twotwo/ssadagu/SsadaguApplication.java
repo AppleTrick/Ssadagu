111b6b1bbc0989d1ce2f4b00ae2b280fca20582b
yejun <jjyejun@naver.com>
2026-02-27 09:58:29 +0900

    setting: 백엔드 초기 파일

---
+++ Ssadagu-Backend/src/main/java/com/twotwo/ssadagu/SsadaguApplication.java
@@ -0,0 +1,13 @@
+package com.twotwo.ssadagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsadaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsadaguApplication.class, args);
+	}
+
+}
---
+++ Ssadagu-Backend/src/test/java/com/twotwo/ssadagu/SsadaguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twotwo.ssadagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsadaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
