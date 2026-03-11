e94de5fc35745e5a39cfcf59415123a2b41a292d
yejun <jjyejun@naver.com>
2026-03-11 14:29:47 +0900

    fix: createdAt 버그 수정

--- Ssadagu-Backend/src/main/java/com/twotwo/ssadagu/SsadaguApplication.java
+++ Ssadagu-Backend/src/main/java/com/twotwo/ssadagu/SsadaguApplication.java
@@ -2,7 +2,9 @@ package com.twotwo.ssadagu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SsadaguApplication {
 
