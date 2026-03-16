359ae066a902508f0d1a5d0f516ba033a648afa8
DESKTOP-6MRTL1B\SSAFY <limsohlha@gmail.com>
2026-03-16 13:27:19 +0900

    feat: 채팅 조회 api 4개 추가

--- Ssadagu-Backend/src/main/java/com/twotwo/ssadagu/domain/chat/repository/ChatRoomRepository.java
+++ Ssadagu-Backend/src/main/java/com/twotwo/ssadagu/domain/chat/repository/ChatRoomRepository.java
@@ -3,8 +3,15 @@ package com.twotwo.ssadagu.domain.chat.repository;
 import com.twotwo.ssadagu.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByProductIdAndBuyerId(Long productId, Long buyerId);
+    
+    List<ChatRoom> findByBuyerIdOrSellerId(Long buyerId, Long sellerId);
+    
+    List<ChatRoom> findByProductId(Long productId);
+    
+    int countByProductId(Long productId);
 }
