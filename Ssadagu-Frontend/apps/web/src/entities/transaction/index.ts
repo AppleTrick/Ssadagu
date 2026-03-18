7982e2147697c534712980f3a1febe3b2e401b9e
Yoonyesol <maniac970430@naver.com>
2026-03-18 14:26:39 +0900

    feat: 상품 및 거래 도메인별 스켈레톤 컴포넌트 구현

--- Ssadagu-Frontend/apps/web/src/entities/transaction/index.ts
+++ Ssadagu-Frontend/apps/web/src/entities/transaction/index.ts
@@ -2,3 +2,4 @@ export { default as TransactionBubble } from './ui/TransactionBubble';
 export { default as HistoryItemCard } from './ui/HistoryItemCard';
 export type { Transaction, Purchase, PaymentMethod, TransactionStatus } from './model/types';
 export { getUserPurchases } from './api/getUserPurchases';
+export { TransactionSkeleton, TransactionListSkeleton } from './ui/TransactionSkeleton';
