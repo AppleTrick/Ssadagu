6891c38783954f4113242e7f2126ae94a8f92b9e
Yoonyesol <maniac970430@naver.com>
2026-03-26 17:40:36 +0900

    fix: 주석처리한 채팅 탭 되살리기

--- Ssadagu-Frontend/apps/web/src/entities/product/ui/ItemCard.tsx
+++ Ssadagu-Frontend/apps/web/src/entities/product/ui/ItemCard.tsx
@@ -70,12 +70,13 @@ const ItemCard = ({ product, onClick, onWishClick }: ItemCardProps) => {
         <Bottom>
           <Price $isSold={isSold}>{formatPrice(product.price)}</Price>
           <StatsContainer>
-            {/* <StatItem>
+            <StatItem>
+              {/* Chat bubble icon */}
               <svg width="15" height="15" viewBox="0 0 24 24" fill={colors.textSecondary}>
                 <path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z" />
               </svg>
               {product.chatCount ?? 0}
-            </StatItem> */}
+            </StatItem>
             <StatItem>
               {/* Heart icon */}
               <WishButton
